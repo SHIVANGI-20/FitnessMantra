4a9bba79a108aad28cc31d566832084deeef3bec
Rohit Choudhary <rohitchoudhary8787@gmail.com>
2021-09-15 20:41:22 -0400

    2nd

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+//home page
 app.get('/', (req, res) => {
    res.send('FitnessMantra');
+});
+
+//about route
+app.get('/about', (req, res) => {
+   res.send('FitnessMantra about page');
 });
 
 const port = process.env.port || 5000;
