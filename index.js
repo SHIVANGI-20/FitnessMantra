224baf57bae1c6da7f32a35339ce34d277991447
Rohit Choudhary <rohitchoudhary8787@gmail.com>
2021-09-22 19:02:41 -0400

    7th

--- index.js
+++ index.js
@@ -13,8 +13,14 @@ app.get('/about', (req, res) => {
    res.send('FitnessMantra about page');
 });
 
-const port = process.env.port || 6000;
+/**const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
    console.log(`Server started on port ${port}`)
+}); **/
+
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
 });
