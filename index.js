d68f1dc04efce00db46de4a81e4cf4b7f7af68e1
Rohit Choudhary <rohitchoudhary8787@gmail.com>
2021-09-22 19:11:20 -0400

    8th

--- index.js
+++ index.js
@@ -19,8 +19,13 @@ app.listen(port, () => {
    console.log(`Server started on port ${port}`)
 }); **/
 
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function() {
-    console.log('Listening on port %d', server_port);
-});
+const host = 'localhost';
+const port = 3000;
+
+const host = '0.0.0.0';
+const port = process.env.PORT || 3000;
+
+
+app.listen(port, host, function() {
+   console.log("Server started.......");
+ });
